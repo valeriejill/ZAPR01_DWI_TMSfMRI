5281a26cefe58f315fb738977bffe94be2abfa20
vsydnor <valeriejill.sydnor@gmail.com>
2020-09-22 11:04:32 -0400

    update sphere size to 3

--- sites_of_stim/siteofstim_sphere_generation.sh
+++ sites_of_stim/siteofstim_sphere_generation.sh
@@ -9,6 +9,7 @@ do
 cat ZAPR01_SiteofStim_MNIcoordinates_BLA.csv | grep $line >> ${line}_siteofstim_BLA_tmp.csv
 cut -d "," -f 3-  ${line}_siteofstim_BLA_tmp.csv >> ${line}_siteofstim_BLA.csv
 rm ${line}_siteofstim_BLA_tmp.csv
+
 cat ZAPR01_SiteofStim_MNIcoordinates_sgACC.csv | grep $line >> ${line}_siteofstim_sgACC_tmp.csv
 cut -d "," -f 3-  ${line}_siteofstim_sgACC_tmp.csv >> ${line}_siteofstim_sgACC.csv
 rm ${line}_siteofstim_sgACC_tmp.csv
@@ -24,7 +25,7 @@ ycoord=$(awk -F, 'BEGIN {OFS=","} {print $2}' ${line}_siteofstim_BLA.csv)
 zcoord=$(awk -F, 'BEGIN {OFS=","} {print $3}' ${line}_siteofstim_BLA.csv)
 
 fslmaths /usr/local/fsl/data/standard/MNI152lin_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi $xcoord 1 $ycoord 1 $zcoord 1 0 1 ${line}_siteofstim_BLA_point.nii.gz -odt float
-fslmaths ${line}_siteofstim_BLA_point.nii.gz -dilM -kernel sphere 5 -fmean -bin ${line}_siteofstim_BLA_ROI.nii.gz
+fslmaths ${line}_siteofstim_BLA_point.nii.gz -dilM -kernel sphere 3 -fmean -bin ${line}_siteofstim_BLA_ROI_3mm.nii.gz
 
 #sgACC
 xcoord=$(awk -F, 'BEGIN {OFS=","} {print $1}' ${line}_siteofstim_sgACC.csv)
@@ -32,7 +33,7 @@ ycoord=$(awk -F, 'BEGIN {OFS=","} {print $2}' ${line}_siteofstim_sgACC.csv)
 zcoord=$(awk -F, 'BEGIN {OFS=","} {print $3}' ${line}_siteofstim_sgACC.csv)
 
 fslmaths /usr/local/fsl/data/standard/MNI152lin_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi $xcoord 1 $ycoord 1 $zcoord 1 0 1 ${line}_siteofstim_sgACC_point.nii.gz -odt float
-fslmaths ${line}_siteofstim_sgACC_point.nii.gz -dilM -kernel sphere 5 -fmean -bin ${line}_siteofstim_sgACC_ROI.nii.gz
+fslmaths ${line}_siteofstim_sgACC_point.nii.gz -dilM -kernel sphere 3 -fmean -bin ${line}_siteofstim_sgACC_ROI_3mm.nii.gz
 
 done < ZAPR01_N65_caselist.txt
 
