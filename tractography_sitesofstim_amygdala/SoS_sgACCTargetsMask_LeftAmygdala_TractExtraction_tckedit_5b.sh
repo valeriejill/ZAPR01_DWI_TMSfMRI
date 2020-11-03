62924753e99fcf8c268665a9d967620a51e7d2ad
vsydnor <valeriejill.sydnor@gmail.com>
2020-11-02 21:12:04 -0500

    add minimum streamline length to exclude outliers

--- tractography_sitesofstim_amygdala/SoS_sgACCTargetsMask_LeftAmygdala_TractExtraction_tckedit_5b.sh
+++ tractography_sitesofstim_amygdala/SoS_sgACCTargetsMask_LeftAmygdala_TractExtraction_tckedit_5b.sh
@@ -2,5 +2,5 @@
 
 ZAP=/storage/vsydnor/ZAPR01_Fixels
 
-/storage/mrtrix3/bin/tckedit $ZAP/Tractography/ZAPR01-FOD-Template-tractography-newparams.tck -include $ZAP/PFC_Amygdala_Tractography_Analyses/ROI_masks/HarvardOxford-sub-maxprob-thr25-1mm-LHamygdala-inFODTemplate.nii.gz -include $ZAP/PFC_Amygdala_Tractography_Analyses/sites_of_stim/sgACC-siteofstim-TargetsMask-inFODTemplate.nii.gz -ends_only $ZAP/PFC_Amygdala_Tractography_Analyses/Tractography/ZAPR01-FOD-Template-sgACCTargetsMask-LHAmygdala-tracts.tck
+/storage/mrtrix3/bin/tckedit $ZAP/Tractography/ZAPR01-FOD-Template-tractography-newparams.tck -include $ZAP/PFC_Amygdala_Tractography_Analyses/ROI_masks/HarvardOxford-sub-maxprob-thr25-1mm-LHamygdala-inFODTemplate.nii.gz -include $ZAP/PFC_Amygdala_Tractography_Analyses/sites_of_stim/sgACC-siteofstim-TargetsMask-inFODTemplate.nii.gz -maxlength 170 -ends_only $ZAP/PFC_Amygdala_Tractography_Analyses/Tractography/ZAPR01-FOD-Template-sgACCTargetsMask-LHAmygdala-tracts.tck
 
